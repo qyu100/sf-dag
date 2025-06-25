4452a0485bbd013809dfb8b711b1d81fc7fbb214
qianyu <qianyu@qianyudeMacBook-Pro.local>
2025-06-25 21:24:38 +0800

    remove log

--- benchmark/benchmark/logs.py
+++ benchmark/benchmark/logs.py
@@ -111,7 +111,6 @@ class LogParser:
             raise ParseError('Primary(s) panicked')
 
         tmp = findall(r'\[(.*Z) .* Created ([^ ]+)\n', log)
-        print(tmp)
         tmp = [(d, self._to_posix(t)) for t, d in tmp]
         proposals = self._merge_results([tmp])
 
