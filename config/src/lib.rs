47ad28f37840a542119c4bc3b32357b1863a3eb6
qianyu <qianyu@qianyudeMacBook-Pro.local>
2025-07-19 13:42:54 +0800

    Fix threshold

--- config/src/lib.rs
+++ config/src/lib.rs
@@ -202,7 +202,7 @@ impl Committee {
         // If N = 3f + 1 + k (0 <= k < 3)
         // then (2 N + 3) / 3 = 2f + 1 + (2k + 2)/3 = 2f + 1 + k = N - f
         let total_votes: Stake = self.authorities.values().map(|x| x.stake).sum();
-        2 * total_votes / 3 + 1
+        2 * self.f_num + 1
     }
 
     pub fn optimistic_threshold(&self) -> Stake {
