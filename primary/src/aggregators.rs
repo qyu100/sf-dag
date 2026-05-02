a0c84ba52c6bbf3f82bfd2a2c3b3015e13faf6dc
qyu100 <qyu100@connect.hkust-gz.edu.cn>
2026-05-02 17:19:09 -0500

    remove propose flag

--- primary/src/aggregators.rs
+++ primary/src/aggregators.rs
@@ -113,7 +113,7 @@ impl CertificatesAggregator {
         // and 2) weight >= max (propose_num - f, 0)
         if self.weight >= committee.quorum_threshold()
             && self.used.contains(&leader)
-            && self.certificate_weight >= propose_num.saturating_sub(committee.f_num as usize) as u32
+            // && self.certificate_weight >= propose_num.saturating_sub(committee.f_num as usize) as u32
         {
             std::thread::sleep(std::time::Duration::from_millis(50));
             self.weight = 0;
@@ -148,7 +148,7 @@ impl CertificatesAggregator {
         // and 2) certificate_weight >= max (propose_num - f, 0)
         if self.weight >= committee.quorum_threshold()
             && self.used.contains(&leader)
-            && self.certificate_weight >= propose_num.saturating_sub(committee.f_num as usize) as u32
+            // && self.certificate_weight >= propose_num.saturating_sub(committee.f_num as usize) as u32
         {
             // std::thread::sleep(std::time::Duration::from_millis(50));
             self.weight = 0;
