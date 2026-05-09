992c863a1a4fc586cb1121d12fdfb7427be0e5d3
qyu100 <qyu100@connect.hkust-gz.edu.cn>
2026-05-08 21:25:41 -0500

    Orphan vertices

--- consensus/src/lib.rs
+++ consensus/src/lib.rs
@@ -160,7 +160,7 @@ impl Consensus {
                             state.insert_certificate(certificate);
                             continue;
                         }
-                        
+
                         ConsensusMessage::Support(support) => {
                             let r = support.round - 1;
                             let leader_round = r;
@@ -179,7 +179,7 @@ impl Consensus {
 
                             let current_stake = self.stake_vote.get(&support.round);
                             let current_stake_value = *current_stake.unwrap_or(&0);
-                            
+
                             // Commit if we have QT
                             if current_stake_value >= self.committee.quorum_threshold() {
                                 debug!("Leader {:?} has enough support with header at round {}", leader, leader_round);
@@ -187,12 +187,18 @@ impl Consensus {
                                     self.collect_commit_sequence(leader, &state).await;
 
                                 // Output the sequence in the right order.
-                                for certificate in sequence {
+                                for (certificate, orphan) in sequence {
                                     state.update(&certificate, self.gc_depth);
                                     #[cfg(not(feature = "benchmark"))]
                                     info!("Committed {} with header", certificate.header_id);
-                                    
-                                    if certificate.round == leader_round {
+
+                                    if orphan {
+                                        info!(
+                                            "Committed {:?} Orphan age {}",
+                                            certificate.header_id,
+                                            leader_round.saturating_sub(certificate.round())
+                                        );
+                                    } else if certificate.round == leader_round {
                                         info!("Committed {:?} Leader", certificate.header_id);
                                     }else if certificate.round == leader_round-1 {
                                         info!("Committed {:?} NonLeader", certificate.header_id);
@@ -214,7 +220,7 @@ impl Consensus {
 
                         ConsensusMessage::HeaderInfo(header_info) => {
                             debug!("Processing header info {:?} at round {}", header_info.id, header_info.round);
-                            
+
                             state.parent_info.insert(header_info.id, header_info.parents.clone());
                             // Try to order the dag to commit. Start from the previous round.
                             let r = header_info.round - 1;
@@ -244,12 +250,18 @@ impl Consensus {
                                     self.collect_commit_sequence(leader, &state).await;
 
                                 // Output the sequence in the right order.
-                                for certificate in sequence {
+                                for (certificate, orphan) in sequence {
                                     state.update(&certificate, self.gc_depth);
                                     #[cfg(not(feature = "benchmark"))]
                                     info!("Committed {} with header", certificate.header_id);
 
-                                    if certificate.round == leader_round {
+                                    if orphan {
+                                        info!(
+                                            "Committed {:?} Orphan age {}",
+                                            certificate.header_id,
+                                            leader_round.saturating_sub(certificate.round())
+                                        );
+                                    } else if certificate.round == leader_round {
                                         info!("Committed {:?} Leader", certificate.header_id);
                                     }else if certificate.round == leader_round-1 {
                                         info!("Committed {:?} NonLeader", certificate.header_id);
@@ -302,7 +314,7 @@ impl Consensus {
         &self,
         leader: &Certificate,
         state: &State,
-    ) -> Vec<Certificate> {
+    ) -> Vec<(Certificate, bool)> {
         let mut sequence = Vec::new();
         let mut already_ordered = HashSet::new();
 
@@ -310,12 +322,12 @@ impl Consensus {
             for certificate in self.order_dag(linked_leader, state).await {
                 let digest = certificate.digest();
                 if already_ordered.insert(digest) {
-                    sequence.push(certificate);
+                    sequence.push((certificate, false));
                 }
             }
         }
 
-        let commit_horizon = leader.round().saturating_sub(1);
+        let commit_horizon = leader.round().saturating_sub(2);
         let pending_candidates = state
             .pending
             .range(..=commit_horizon)
@@ -326,17 +338,44 @@ impl Consensus {
             if already_ordered.contains(&digest) {
                 continue;
             }
+            if self.referenced_by_next_round(&certificate, state).await {
+                continue;
+            }
 
             for candidate in self.order_dag(&certificate, state).await {
                 let candidate_digest = candidate.digest();
                 if already_ordered.insert(candidate_digest) {
-                    sequence.push(candidate);
+                    let candidate_is_orphan = candidate.header_id == certificate.header_id;
+                    sequence.push((candidate, candidate_is_orphan));
                 }
             }
         }
 
-        sequence.sort_by_key(|certificate| certificate.round());
+        sequence.sort_by_key(|(certificate, _)| certificate.round());
         sequence
+    }
+
+    async fn referenced_by_next_round(&self, certificate: &Certificate, state: &State) -> bool {
+        let next_round = certificate.round().saturating_add(1);
+        let Some(next_round_certificates) = state.dag.get(&next_round) else {
+            return false;
+        };
+
+        for (_, next_round_certificate) in next_round_certificates.values() {
+            let parents = loop {
+                if let Some(parents) = state.parent_info.get(&next_round_certificate.header_id) {
+                    break parents;
+                }
+
+                sleep(Duration::from_millis(1)).await;
+            };
+
+            if parents.contains(&certificate.header_id) {
+                return true;
+            }
+        }
+
+        false
     }
 
     /// Order the past leaders that we didn't already commit.
@@ -360,12 +399,7 @@ impl Consensus {
     }
 
     /// Checks if there is a path between two leaders.
-    async fn linked(
-        &self,
-        leader: &Certificate,
-        prev_leader: &Certificate,
-        state: &State,
-    ) -> bool {
+    async fn linked(&self, leader: &Certificate, prev_leader: &Certificate, state: &State) -> bool {
         let mut parents = vec![leader];
         for r in (prev_leader.round()..leader.round()).rev() {
             let round_certificates = state
@@ -383,7 +417,7 @@ impl Consensus {
                                 new_parents.push(certificate);
                                 break;
                             } else {
-                                break; 
+                                break;
                             }
                         } else {
                             sleep(Duration::from_millis(1)).await;
@@ -541,8 +575,12 @@ mod tests {
             state.insert_certificate(certificate);
         }
 
-        state.parent_info.insert(round_1_leader.header_id, Vec::new());
-        state.parent_info.insert(round_1_orphan.header_id, Vec::new());
+        state
+            .parent_info
+            .insert(round_1_leader.header_id, Vec::new());
+        state
+            .parent_info
+            .insert(round_1_orphan.header_id, Vec::new());
         state
             .parent_info
             .insert(round_2_leader.header_id, vec![round_1_leader.header_id]);
@@ -554,12 +592,11 @@ mod tests {
             .collect_commit_sequence(&round_3_leader, &state)
             .await;
 
-        assert!(sequence.iter().any(|x| x.header_id == round_1_orphan.header_id));
+        assert!(sequence
+            .iter()
+            .any(|(x, orphan)| x.header_id == round_1_orphan.header_id && *orphan));
         assert_eq!(
-            sequence
-                .iter()
-                .map(|x| x.round())
-                .collect::<Vec<_>>(),
+            sequence.iter().map(|(x, _)| x.round()).collect::<Vec<_>>(),
             vec![1, 1, 2, 3]
         );
     }
